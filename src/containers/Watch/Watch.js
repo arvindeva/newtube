2d51f20db5e9fccf7e7042c9f59dcf55f63a9ecc
arvindeva <arvindeva@gmail.com>
2019-02-18 21:14:13 +0700

    Add video component and it's responsive

--- src/containers/Watch/Watch.js
+++ src/containers/Watch/Watch.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './Watch.scss';
 
+import Video from '../../components/Video/Video';
+
 class Watch extends React.Component {
   render() {
     return (
-      <div>Watch Component</div>
+      <div style={{ maxWidth: '80%' }}>
+        <Video id="-7fuHEEmEjs"/>
+      </div>
     );
   }
 }
