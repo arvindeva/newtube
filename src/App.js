2701afcb092ada9e62418ea434805740e368189b
arvindeva <arvindeva@gmail.com>
2019-02-18 13:05:31 +0700

    Done with header navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 
+import HeaderNav from './containers/HeaderNav/HeaderNav'
+
 class App extends Component {
   render() {
     return (
-      <div>Hello World</div>
+      <div>
+        <HeaderNav />
+      </div>
     );
   }
 }
