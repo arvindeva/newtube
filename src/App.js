11a9bdc1c31dc18440a156f240d6e2f5133c9a7d
arvindeva <arvindeva@gmail.com>
2019-02-18 20:40:49 +0700

    Added a Watch component and React Router to organize routes, and fixed App test

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 
 import AppLayout from './components/AppLayout/AppLayout';
 import Home from './containers/Home/Home';
+import Watch from './containers/Watch/Watch';
 import './containers/Home/Home.scss';
 
 class App extends Component {
   render() {
     return (
       <AppLayout>
-        <Home />
+        <Switch>
+          <Route path="/watch" component={Watch} />
+          <Route path="/" exact component={Home} />
+        </Switch>
       </AppLayout>
     );
   }
---
+++ src/containers/Watch/Watch.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Watch.scss';
+
+class Watch extends React.Component {
+  render() {
+    return (
+      <div>Watch Component</div>
+    );
+  }
+}
+
+export default Watch;
