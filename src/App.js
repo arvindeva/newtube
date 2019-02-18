c40a23ec60b13aa8b32b746fcb914993ef54b80e
arvindeva <arvindeva@gmail.com>
2019-02-18 19:06:40 +0700

    Added home container and finished VideoPreview

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 
 import HeaderNav from './containers/HeaderNav/HeaderNav';
 import SideBar from './containers/SideBar/SideBar';
+import Home from './containers/Home/Home';
+import './containers/Home/Home.scss';
 
 class App extends Component {
   render() {
@@ -9,6 +11,7 @@ class App extends Component {
       <React.Fragment>
         <HeaderNav />
         <SideBar />
+        <Home />
       </React.Fragment>
     );
   }
---
+++ src/containers/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import VideoPreview from '../../components/VideoPreview/VideoPreview';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div className="home">
+        <VideoPreview />
+      </div>
+    );
+  }
+}
+
+export default Home;
